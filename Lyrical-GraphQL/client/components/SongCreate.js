fbaeb983f3dac92eaa86c16ce19b3e8d4dd27f88
Kevin Sparling <kevinsparling@gmail.com>
2019-11-24 13:45:55 +0800

    refactored queries, added graphql-tag

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
  
 class SongCreate extends Component {
     constructor(props) {
@@ -6,11 +9,24 @@ class SongCreate extends Component {
 
         this.state = { title: '' };
     }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        this.props.mutate({
+            variables: {
+                title: this.state.title,
+                refetchQueies: [{ query }]
+            }
+        }).then(() => hashHistory.push('/'));
+    }
+
     render() {
         return (
             <div>
+                <Link to="/">Back</Link>
                 <h3>Create a New Song</h3>
-                <form>
+                <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
                     <input
                         onChange={event => this.setState({ title: event.target.value })}
@@ -22,4 +38,12 @@ class SongCreate extends Component {
     }
 }
 
-export default SongCreate;
+const mutation = gql`
+    mutation AddSong($title: String){
+        addSong(title: $title){
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
