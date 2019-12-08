b7c13434a87798b18ad75e844a826b30538b4144
Kevin Sparling <kevinsparling@gmail.com>
2019-12-08 11:45:05 +0800

    halfway through Section 13

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+    renderButtons() {
+        const { loading, user } = this.props.data;
+
+        if (loading) {
+            return <div />;
+        }
+
+        if (user) {
+            return <div>Logout</div>;
+        } else {
+            return (
+                <div>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </div>
+            );
+        }
+    }
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to="/" className="brand-logo left">
+                        Home
+                    </Link>
+                   {this.renderButtons()} 
+                </div>
+            </nav>
         );
     }
 }
